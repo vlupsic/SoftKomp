e8dce1556a5361383ab12d3e8f15ab1ac229ccbb
vlupsic <vasilije.lupsic45@gmail.com>
2024-11-04 01:15:43 +0100

    valjda sam uspeo sad

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
-rootProject.name = "MrtviProjekat2"
+rootProject.name = "SoftKompProject"
 include("specifikacija")
 include("kalkulacije")
 include("TXTImplementacija")
