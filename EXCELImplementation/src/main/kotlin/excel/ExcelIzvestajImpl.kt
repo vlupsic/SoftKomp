a2ab5c7969c8b8eba15547b2e8a20c694333b3a6
vlupsic <vasilije.lupsic45@gmail.com>
2024-11-17 02:18:00 +0100

    excel polovicno tu

--- EXCELImplementation/src/main/kotlin/excel/ExcelIzvestajImpl.kt
+++ EXCELImplementation/src/main/kotlin/excel/ExcelIzvestajImpl.kt
@@ -1,5 +1,7 @@
 package excel
 
+import com.google.gson.Gson
+import model.ExcelFormat
 import org.apache.poi.ss.usermodel.*
 import org.apache.poi.ss.util.CellRangeAddress
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
@@ -20,6 +22,70 @@ class ExcelIzvestajImpl() : IzvestajInterface {
         summary: String?,
         fajl: File
     ) {
+        try {
+            val format = formatConf(fajl)
+            val excel = excel(podaci, header, naslov, summary, format)
+            FileOutputStream(destinacija).use { outputStream ->
+            excel.write(outputStream)
+           }
+            excel.close()
+        }catch (e: Exception) {
+            e.printStackTrace()
+            println("Greska pri generisanju EXCEL-a: ${e.message}")
+        }
+    }
+
+    private fun formatConf(conf: File): ExcelFormat? {
+        return try {
+            val gson = Gson()
+
+            conf.reader().use { reader ->
+                gson.fromJson(reader, ExcelFormat::class.java)
+            }
+        }catch (e:Exception){
+            println("Error loading config file: ${e.message}")
+            null
+        }
+    }
+
+    private fun stringToAlignment(alignment: String): HorizontalAlignment {
+        return when (alignment.lowercase()) {
+            "center" -> HorizontalAlignment.CENTER
+            "left" -> HorizontalAlignment.LEFT
+            "right" -> HorizontalAlignment.RIGHT
+            "justify" -> HorizontalAlignment.JUSTIFY
+            else -> HorizontalAlignment.GENERAL
+        }
+    }
+
+    private fun applyStyleToRow(row: Row, style: CellStyle, numColumns: Int) {
+        for (i in 0 until numColumns) {
+            val cell = row.getCell(i)
+            cell.cellStyle = style
+        }
+    }
+
+    fun stringToBorderStyle(style: String): BorderStyle {
+        return when (style.lowercase()) {
+            "thin" -> BorderStyle.THIN
+            "thick" -> BorderStyle.THICK
+            "dotted" -> BorderStyle.DOTTED
+            "dashed" -> BorderStyle.DASHED
+            "double" -> BorderStyle.DOUBLE
+            "medium" -> BorderStyle.MEDIUM
+            "medium_dashed" -> BorderStyle.MEDIUM_DASHED
+            "none" -> BorderStyle.NONE
+            else -> BorderStyle.NONE
+        }
+    }
+
+    private fun excel(
+        podaci: MutableMap<String, MutableList<String>>,
+        header: Boolean,
+        naslov: String?,
+        summary: String?,
+        format: ExcelFormat?
+    ) :Workbook{
         val workbook: Workbook = XSSFWorkbook()
         val sheet: Sheet = workbook.createSheet("Report")
 
@@ -29,10 +95,16 @@ class ExcelIzvestajImpl() : IzvestajInterface {
             titleCell.setCellValue(it)
             sheet.addMergedRegion(CellRangeAddress(0,0,0, podaci.size - 1))
             val titleStyle = workbook.createCellStyle().apply {
-                alignment = HorizontalAlignment.CENTER
+                if (format != null) {
+                    alignment = stringToAlignment(format.naslov.alignment)
+                }
                 val titleFont = workbook.createFont().apply {
-                    bold = true
-                    fontHeightInPoints = 18
+                    if (format != null) {
+                        bold = format.naslov.bold
+                        italic = format.naslov.italic
+                        fontHeightInPoints = format.naslov.fontSize.toShort()
+                        color = format.naslov.color.toShort()
+                    }
                 }
                 this.setFont(titleFont)
             }
@@ -42,7 +114,29 @@ class ExcelIzvestajImpl() : IzvestajInterface {
             val headerRow : Row = sheet.createRow(1)
             podaci.keys.forEachIndexed { index, columnName ->
                 headerRow.createCell(index).setCellValue(columnName)
+                headerRow.rowStyle = workbook.createCellStyle().apply {
+                    val redFont = workbook.createFont().apply {
+                        if (format != null) {
+                            bold = format.header.bold
+                            italic = format.header.italic
+                            underline = if (format.header.underline) Font.U_SINGLE else Font.U_NONE
+                            fontHeight = format.header.fontSize.toShort()
+                            color = format.header.color.toShort()
+                            borderTop = stringToBorderStyle(format.header.borderStyle)
+                            borderRight = stringToBorderStyle(format.header.borderStyle)
+                            borderLeft = stringToBorderStyle(format.header.borderStyle)
+                            borderBottom = stringToBorderStyle(format.header.borderStyle)
+                            topBorderColor = format.header.borderColor.toShort()
+                            leftBorderColor = format.header.borderColor.toShort()
+                            rightBorderColor = format.header.borderColor.toShort()
+                            bottomBorderColor = format.header.borderColor.toShort()
+                            alignment = stringToAlignment(format.header.alignment)
+                        }
+                    }
+                    this.setFont(redFont)
+                }
             }
+
         }
         val numRows = podaci.values.first().size
         for (i in 1..numRows){
@@ -54,13 +148,9 @@ class ExcelIzvestajImpl() : IzvestajInterface {
         summary?.let {
             val summaryRow : Row = sheet.createRow(numRows + 2)
             val summaryCell : Cell = summaryRow.createCell(0)
-            summaryCell.setCellValue("Summary: $it")
+            summaryCell.setCellValue("Summary: $summary")
         }
-        FileOutputStream(destinacija).use { outputStream ->
-            workbook.write(outputStream)
-        }
-        workbook.close()
-
+        return workbook
     }
 
 }
